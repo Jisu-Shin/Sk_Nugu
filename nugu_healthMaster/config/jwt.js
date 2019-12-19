62d395110fb7881a0f1543851d4e86c90a127d84
zsu <sjszzang0929@naver.com>
2019-12-20 00:33:18 +0900

    update file

--- nugu_healthMaster/config/jwt.js
+++
@@ -1,6 +0,0 @@
-let jwtObj = {};
-
-jwtObj.client_secret = "UGVyQXJkdWFBZEFzdHJh" // PerArduaAdAstra
-jwtObj.client_id = "Q2FtZWxpYVByb2plY3RZVW5pdA==" // CameliaProjectYUnit
-
-module.exports = jwtObj
--- nugu_healthMaster/jscode/authmail.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = function (min, max) {
-  var ranNum = Math.floor(Math.random()*(max-min+1)) + min;
-  return ranNum;
-}
